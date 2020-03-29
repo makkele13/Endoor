13e2d7e4056a2a74804d34e73c1ad4c7fa8cef2b
Makkele13 <info@markwaarde.nl>
2020-03-29 22:21:44 +0200

    Include opdracht 1.2

---
+++ Opdracht_1_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opdracht_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
